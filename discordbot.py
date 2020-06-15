608c5d166d2358aa98d0bcbc578c1a2cf6a8a48d
Mig3 <65160722+mig@users.noreply.github.com>
2020-06-15 18:04:36 +0900

    yajuコマンド追加
    (首)

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,7 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
-
+    if message.content == '/yaju':
+        await message.channel.send('ｲｷｽｷﾞｨ!!ｲｸｯｲｸｯ...ﾍｯﾍｯﾍｯ...ﾝｱｰｯ!!')
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
