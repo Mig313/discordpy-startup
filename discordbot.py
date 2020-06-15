02f913f2268ada83f1e82ff9412b264f7210c753
Mig3 <65160722+mig@users.noreply.github.com>
2020-06-15 21:29:35 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -15,22 +15,16 @@ async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
 
+# 発言時に実行されるイベントハンドラを定義
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-    if message.content == '/yaju':
-        await message.channel.send('ｲｷｽｷﾞｨ!!ｲｸｯｲｸｯ...ﾍｯﾍｯﾍｯ...ﾝｱｰｯ!!')
-    if message.content == "/op":
-        member = message.autho
-        role = member.guild.get_role(722022742123348020)
-        await member.add_roles(role)
-        #role = guild.get_role(722022742123348020)
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
