7c3670f021482d4a50670f454916cebb28cb8ed6
Mig3 <65160722+mig@users.noreply.github.com>
2020-06-17 13:57:29 +0900

    さいしょーこーせー

--- discordbot.py
+++ discordbot.py
@@ -55,7 +55,7 @@ async def  on_message_edit(before,after):
 
 @client.event
 async def on_raw_reaction_add(payload):
-    channel = guild.get_channel(718096349140353034)
+    channel = discord.Guild.get_channel(718096349140353034)
     await channel.send('Debug!!!')
 #    ch_id = 718096349140353034#送信先チャンネル
 #    roles_id_ = 722630435707813888#変態役職
